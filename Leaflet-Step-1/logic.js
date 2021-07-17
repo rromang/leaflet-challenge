c30285dd3861f6d2b933e8b59d05159f5d67a8bd
rromang <80008461+rromang@users.noreply.github.com>
2021-07-17 11:55:30 -0700

    Update logic.js

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -40,6 +40,31 @@ function createFeatures(earthquakeData) {
   // console.log(coords);
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
+
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h1>Median Income</h1>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
 }
 
 function createMap(earthquakes) {
